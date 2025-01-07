88edb2f1f850d98ec34499f4cbca0d245d3ba041
pws <soyul20.papa@gmail.com>
2025-01-07 15:55:23 +0900

    github action setting

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -33,6 +33,18 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'eqeqeq': 'error',
+      'no-unused-vars': 'off',
+      'curly': 'off',
+      'no-console': 'warn',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'max-len': ['warn', { code: 80 }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-no-duplicate-props': 'error',
+      'react/jsx-uses-react': 'off',
     },
   },
-]
+];
