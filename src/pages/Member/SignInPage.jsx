b0ebfc2f1148d7f16ccbed3660618b3e4e0a495c
Ki-Joon PARK <126080146+pearl-jam@users.noreply.github.com>
2025-06-01 22:49:16 +0900

    chore: 로그인 디자인 수정

--- src/pages/Member/SignInPage.jsx
+++ src/pages/Member/SignInPage.jsx
@@ -143,7 +143,7 @@ const SignInPage = () => {
 
   return (
     <>
-      <section className="big-section bg-very-light-gray pt-4 md-mt-60px">
+      <section className="section bg-very-light-gray pt-6 md-mt-60px pb-1">
         <div className="container">
           <div className="row g-0 justify-content-center bg-white">
             <div
@@ -161,184 +161,192 @@ const SignInPage = () => {
               className="col-lg-8 col-md-10 p-5"
               data-anime='{ "translateY": [0, 0], "opacity": [0,1], "duration": 600, "delay":150, "staggervalue": 150, "easing": "easeOutQuad" }'
             >
-              <form>
-                <label className="text-dark-gray mb-10px md-mb-0 fw-500">
-                  이메일<span className="text-red">*</span>
-                </label>
-                <input
-                  className="mb-10px bg-very-light-white form-control required"
-                  type="email"
-                  name="loginEmail"
-                  value={member.loginEmail}
-                  onChange={handleMemberRegisterChange}
-                  placeholder="이메일을 입력 하세요"
-                />
-                {errors.loginEmail && (
-                  <p className="text-danger">{errors.loginEmail}</p>
-                )}
+              <form className="px-4">
+                <div className="mb-4">
+                  <label className="text-dark-gray mb-2 d-block fw-500">
+                    이메일<span className="text-red">*</span>
+                  </label>
+                  <input
+                    className="form-control required py-3"
+                    type="email"
+                    name="loginEmail"
+                    value={member.loginEmail}
+                    onChange={handleMemberRegisterChange}
+                    placeholder="이메일을 입력하세요"
+                  />
+                  {errors.loginEmail && (
+                    <p className="text-danger mt-2 mb-0 fs-14">{errors.loginEmail}</p>
+                  )}
+                </div>
 
-                <label className="text-dark-gray mb-10px md-mb-0 fw-500">
-                  비밀번호<span className="text-red">*</span>
-                </label>
-                <input
-                  className="mb-10px bg-very-light-white form-control required"
-                  type="password"
-                  name="password"
-                  value={member.password}
-                  onChange={handleMemberRegisterChange}
-                  placeholder="비밀번호를 입력하세요"
-                />
-                {errors.password && (
-                  <p className="text-danger">{errors.password}</p>
-                )}
+                <div className="mb-4">
+                  <label className="text-dark-gray mb-2 d-block fw-500">
+                    비밀번호<span className="text-red">*</span>
+                  </label>
+                  <input
+                    className="form-control required py-3"
+                    type="password"
+                    name="password"
+                    value={member.password}
+                    onChange={handleMemberRegisterChange}
+                    placeholder="비밀번호를 입력하세요"
+                  />
+                  {errors.password && (
+                    <p className="text-danger mt-2 mb-0 fs-14">{errors.password}</p>
+                  )}
+                </div>
 
                 <input type="hidden" name="redirect" value="" />
-                <div className="text-center">
+                <div className="text-center mt-4 mb-4">
                   <Button
                     type="submit"
                     size="extra-large"
                     radiusOn="radius-on"
-                    className="btn btn-medium btn-round-edge btn-base-color btn-box-shadow w-100 md-w-50 text-transform-none"
+                    className="btn btn-medium btn-round-edge btn-base-color btn-box-shadow w-100 md-w-50 text-transform-none py-3"
                     onClick={handleLogin}
                   >
                     로그인
                   </Button>
                 </div>
-                <div className="pt-4_5 md-pt-5 md-pb-0 text-center">
-                  <Link to="/signup" className="ps-25px">
+                <div className="text-center mb-5">
+                  <Link to="/signup" className="text-dark-gray text-decoration-none">
                     회원가입
                   </Link>
-                  <span className="px-5">|</span>
-                  <Link to="/password-forgot">비밀번호찾기</Link>
+                  <span className="mx-3 text-dark-gray">|</span>
+                  <Link to="/password-forgot" className="text-dark-gray text-decoration-none">
+                    비밀번호찾기
+                  </Link>
                 </div>
-                <div className="pt-40px md-pt-2 text-center d-flex flex-row align-items-center">
-                  <Button
-                    data-value="google"
-                    name="google"
-                    size="extra-large"
-                    color="google"
-                    className="btn-large w-50 md-w-100 d-block"
-                    onClick={handleSnsLoginAction}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="38"
-                      height="38"
-                      viewBox="0 0 38 38"
+                <div className="text-center">
+                  <div className="d-flex justify-content-center gap-3">
+                    <Button
+                      data-value="google"
+                      name="google"
+                      size="extra-large"
+                      color="google"
+                      className="btn-large w-100 d-block py-3"
+                      onClick={handleSnsLoginAction}
                     >
-                      <g fill="none" fillRule="evenodd">
-                        <rect x="0" y="0" width="38" height="38" fill="#FFF" />
-                        <g>
-                          <path
-                            d="M0 0H24V24H0z"
-                            transform="translate(7 7)"
-                          ></path>
-                          <g fillRule="nonzero">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="38"
+                        height="38"
+                        viewBox="0 0 38 38"
+                      >
+                        <g fill="none" fillRule="evenodd">
+                          <rect x="0" y="0" width="38" height="38" fill="#FFF" />
+                          <g>
                             <path
-                              fill="#4285F4"
-                              d="M16.735 8.733c0-.606-.054-1.188-.155-1.747H8.538v3.303h4.596c-.198 1.067-.8 1.971-1.704 2.577v2.142h2.76c1.614-1.486 2.545-3.675 2.545-6.275z"
-                              transform="translate(7 7) translate(3.462 3.462)"
+                              d="M0 0H24V24H0z"
+                              transform="translate(7 7)"
+                            ></path>
+                            <g fillRule="nonzero">
+                              <path
+                                fill="#4285F4"
+                                d="M16.735 8.733c0-.606-.054-1.188-.155-1.747H8.538v3.303h4.596c-.198 1.067-.8 1.971-1.704 2.577v2.142h2.76c1.614-1.486 2.545-3.675 2.545-6.275z"
+                                transform="translate(7 7) translate(3.462 3.462)"
+                              ></path>
+                              <path
+                                fill="#34A853"
+                                d="M8.538 17.077c2.306 0 4.239-.765 5.651-2.069l-2.76-2.142c-.764.512-1.742.815-2.89.815-2.224 0-4.107-1.502-4.778-3.52H.908v2.212c1.405 2.79 4.293 4.704 7.63 4.704z"
+                                transform="translate(7 7) translate(3.462 3.462)"
+                              ></path>
+                              <path
+                                fill="#FBBC05"
+                                d="M3.76 10.16c-.17-.512-.267-1.059-.267-1.622 0-.562.097-1.11.268-1.622V4.704H.908C.33 5.857 0 7.16 0 8.538c0 1.378.33 2.682.908 3.835l2.853-2.212z"
+                                transform="translate(7 7) translate(3.462 3.462)"
+                              ></path>
+                              <path
+                                fill="#EA4335"
+                                d="M8.538 3.396c1.254 0 2.38.43 3.264 1.277l2.45-2.45C12.771.847 10.84 0 8.537 0 5.201 0 2.313 1.913.908 4.704l2.853 2.212c.671-2.018 2.554-3.52 4.777-3.52z"
+                                transform="translate(7 7) translate(3.462 3.462)"
+                              ></path>
+                            </g>
+                          </g>
+                        </g>
+                      </svg>
+                      <span className="ps-10px d-lg-inline-block d-md-none d-sm-none">
+                        구글 로그인
+                      </span>
+                    </Button>
+                    <Button
+                      data-value="kakao"
+                      size="extra-large"
+                      color="kakao"
+                      className="btn-large w-100 d-block py-3"
+                      onClick={handleSnsLoginAction}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="38"
+                        height="38"
+                        viewBox="0 0 38 38"
+                      >
+                        <g fill="none" fillRule="evenodd">
+                          <rect
+                            x="0"
+                            y="0"
+                            width="38"
+                            height="38"
+                            fill="#FAE400"
+                          />
+                          <g>
+                            <path
+                              d="M0 0H24V24H0z"
+                              transform="translate(7 7)"
                             ></path>
                             <path
-                              fill="#34A853"
-                              d="M8.538 17.077c2.306 0 4.239-.765 5.651-2.069l-2.76-2.142c-.764.512-1.742.815-2.89.815-2.224 0-4.107-1.502-4.778-3.52H.908v2.212c1.405 2.79 4.293 4.704 7.63 4.704z"
-                              transform="translate(7 7) translate(3.462 3.462)"
-                            ></path>
-                            <path
-                              fill="#FBBC05"
-                              d="M3.76 10.16c-.17-.512-.267-1.059-.267-1.622 0-.562.097-1.11.268-1.622V4.704H.908C.33 5.857 0 7.16 0 8.538c0 1.378.33 2.682.908 3.835l2.853-2.212z"
-                              transform="translate(7 7) translate(3.462 3.462)"
-                            ></path>
-                            <path
-                              fill="#EA4335"
-                              d="M8.538 3.396c1.254 0 2.38.43 3.264 1.277l2.45-2.45C12.771.847 10.84 0 8.537 0 5.201 0 2.313 1.913.908 4.704l2.853 2.212c.671-2.018 2.554-3.52 4.777-3.52z"
-                              transform="translate(7 7) translate(3.462 3.462)"
+                              fill="#3C1E1E"
+                              fillRule="nonzero"
+                              d="M11.992 3.692c5.093 0 9.222 3.303 9.222 7.377s-4.129 7.376-9.222 7.376c-.552 0-1.092-.038-1.616-.113l-3.64 2.512c-.172.12-.368-.021-.343-.193.014-.102.27-1.202.765-3.299-2.633-1.298-4.389-3.627-4.389-6.283 0-4.074 4.13-7.377 9.223-7.377z"
+                              transform="translate(7 7)"
                             ></path>
                           </g>
                         </g>
-                      </g>
-                    </svg>
-                    <span className="ps-10px d-lg-inline-block d-md-none d-sm-none">
-                      구글 로그인
-                    </span>
-                  </Button>
-                  <Button
-                    data-value="kakao"
-                    size="extra-large"
-                    color="kakao"
-                    className="btn-large w-50 md-w-100 d-block"
-                    onClick={handleSnsLoginAction}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="38"
-                      height="38"
-                      viewBox="0 0 38 38"
+                      </svg>
+                      <span className="ps-10px d-lg-inline-block d-md-none d-sm-none">
+                        카카오 로그인
+                      </span>
+                    </Button>
+                    <Button
+                      data-value="naver"
+                      size="extra-large"
+                      color="naver"
+                      className="btn-large w-100 d-block py-3"
+                      onClick={handleSnsLoginAction}
                     >
-                      <g fill="none" fillRule="evenodd">
-                        <rect
-                          x="0"
-                          y="0"
-                          width="38"
-                          height="38"
-                          fill="#FAE400"
-                        />
-                        <g>
-                          <path
-                            d="M0 0H24V24H0z"
-                            transform="translate(7 7)"
-                          ></path>
-                          <path
-                            fill="#3C1E1E"
-                            fillRule="nonzero"
-                            d="M11.992 3.692c5.093 0 9.222 3.303 9.222 7.377s-4.129 7.376-9.222 7.376c-.552 0-1.092-.038-1.616-.113l-3.64 2.512c-.172.12-.368-.021-.343-.193.014-.102.27-1.202.765-3.299-2.633-1.298-4.389-3.627-4.389-6.283 0-4.074 4.13-7.377 9.223-7.377z"
-                            transform="translate(7 7)"
-                          ></path>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="38"
+                        height="38"
+                        viewBox="0 0 38 38"
+                      >
+                        <g fill="none" fillRule="evenodd">
+                          <rect
+                            x="0"
+                            y="0"
+                            width="38"
+                            height="38"
+                            fill="#00C73C"
+                          />
+                          <g>
+                            <path
+                              d="M0 0H24V24H0z"
+                              transform="translate(7 7)"
+                            ></path>
+                            <path
+                              d="M12.74 10.537l-5.7-8.193H2.312v15.303h4.953V9.454l5.7 8.193h4.727v-15.3h-4.953v8.19z"
+                              fill="#FFF"
+                              transform="translate(9 9)"
+                            ></path>
+                          </g>
                         </g>
-                      </g>
-                    </svg>
-                    <span className="ps-10px d-lg-inline-block d-md-none d-sm-none">
-                      카카오 로그인
-                    </span>
-                  </Button>
-                  <Button
-                    data-value="naver"
-                    size="extra-large"
-                    color="naver"
-                    className="btn-large w-50 md-w-100 d-block"
-                    onClick={handleSnsLoginAction}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="38"
-                      height="38"
-                      viewBox="0 0 38 38"
-                    >
-                      <g fill="none" fillRule="evenodd">
-                        <rect
-                          x="0"
-                          y="0"
-                          width="38"
-                          height="38"
-                          fill="#00C73C"
-                        />
-                        <g>
-                          <path
-                            d="M0 0H24V24H0z"
-                            transform="translate(7 7)"
-                          ></path>
-                          <path
-                            d="M12.74 10.537l-5.7-8.193H2.312v15.303h4.953V9.454l5.7 8.193h4.727v-15.3h-4.953v8.19z"
-                            fill="#FFF"
-                            transform="translate(9 9)"
-                          ></path>
-                        </g>
-                      </g>
-                    </svg>
-                    <span className="ps-10px d-lg-inline-block d-md-none d-sm-none">
-                      네이버 로그인
-                    </span>
-                  </Button>
+                      </svg>
+                      <span className="ps-10px d-lg-inline-block d-md-none d-sm-none">
+                        네이버 로그인
+                      </span>
+                    </Button>
+                  </div>
                 </div>
               </form>
             </div>
